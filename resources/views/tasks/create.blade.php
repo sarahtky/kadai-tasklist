a6658c7048ad2daa1e7b06e816d2703d4e367845
sarahtky <sarahfrederica30@gmail.com>
2018-09-03 13:16:20 +0000

    bootstrap design update2

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,29 +6,20 @@
 <h1>タスク新規作成ページ</h1>
 
 <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-sd-offset-2 col-sd-8 col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+    <input type="select" class="dropdown-menu">
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::select('status', ['New', 'Working', 'Waiting', 'Done'], null, ['class' => 'field'])!!}
 
-                <div class="dropdown">
-                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                ステータス<span class="caret"></span>
-                 </button>
-                  <!-- 選択肢 -->
-                  <ul class="dropdown-menu">
-                    <li><a href="#">New</a></li>
-                    <li><a href="#">Working</a></li>
-                    <li><a href="#">Waiting</a></li>
-                    <li><a href="#">Done</a></li>
-                  </ul>
-                </div>
-                
-                <div class="form-group">
-                    {!! Form::label('content', 'タスク:') !!}
-                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                </div>
+        <div class="form-group">
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
 
-                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+            {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
             {!! Form::close() !!}
             
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,29 +5,20 @@
 <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
 <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-sd-offset-2 col-sd-8 col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
-                <div class="dropdown">
-                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                ステータス<span class="caret"></span>
-                 </button>
-                  <!-- 選択肢 -->
-                  <ul class="dropdown-menu">
-                    <li><a href="#">New</a></li>
-                    <li><a href="#">Working</a></li>
-                    <li><a href="#">Waiting</a></li>
-                    <li><a href="#">Done</a></li>
-                  </ul>
-                </div>
+    <input type="select" class="dropdown-menu">
+    {!! Form::label('status', 'ステータス:') !!}
+    {!! Form::select('status', ['New', 'Working', 'Waiting', 'Done'], null, ['class' => 'field'])!!}
+
+    <div class="form-group">
+    {!! Form::label('content', 'タスク:') !!}
+    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+    </div>
                 
-                <div class="form-group">
-                    {!! Form::label('content', 'タスク:') !!}
-                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
-                </div>
-                
-                        {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
+            {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 
             {!! Form::close() !!}
             
